15d19339e1949f9a00787755d10682dc6733bb38
victoriaaoka <victoriaaoka@gmail.com>
2019-05-30 15:19:22 +0300

    add access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   constructor(private http: HttpClient) { }
   httpdata;
   ngOnInit() {
-    this.http.get("https://api.unsplash.com/photos/?client_id=YOUR-UNSPLUSH-ACCESS-KEY")
+    this.http.get("https://api.unsplash.com/photos/?client_id=ef4edec5249da190641f94de3e099d61bb8be002e8056471b83d87065c2a212d")
       .subscribe(data => this.displaydata(data))
   }
   displaydata(data) {
